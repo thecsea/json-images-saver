40e884cecd73a317a0e2b899437f05afd47b1d20
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-08-25 01:08:07 +0200

    Revert "[gulp] important fixes"
    This reverts commit f9602490733920ccfc694219da5a1edcca41e864.

--- elixir.js
+++ elixir.js
@@ -1,11 +1,12 @@
 /**
  * Created by claudio on 25/08/16.
  */
-var Elixir = require('../laravel-elixir');
-var Gulp = require('../gulp');
+var Elixir = require('laravel-elixir');
+var Gulp = require('gulp');
 
-var Print = require('../gulp-print');
-var Plumber = require('../gulp-plumber');
+var Print = require('gulp-print');
+var Plumber = require('gulp-plumber');
+
 var imagesSaver = require('./gulp');
 
 var Task = Elixir.Task;
